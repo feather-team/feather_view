0877c9503fd7c4c319560f1d15b91c5f9d4c40f2
Zhang hao <jsyczhanghao@sina.com>
2014-12-19 23:02:16 +0800

    优化逻辑，去掉notice

--- plugins/feather_view_autoload_static.plugin.php
+++ plugins/feather_view_autoload_static.plugin.php
@@ -8,13 +8,18 @@ opt: array(
 )
 */
 function feather_view_autoload_static($path, $content = '', $view, $opt = array()){
-	if($root = $opt['domain']){
-		$view->set('FEATHER_STATIC_DOMAIN', $root);
+	$root = '';
+	$cache = null;
+
+	if(isset($opt['domain'])){
+		$root = $opt['domain'];
 	}
+
+	$view->set('FEATHER_STATIC_DOMAIN', $root);
 	
 	$path = '/' . ltrim($path, '/');
 
-	if($opt['cache_dir']){
+	if(isset($opt['cache_dir'])){
 		$md5path = rtrim($opt['cache_dir'], '/') . '/' . md5($path) . '.php';
 
 		if(is_file($md5path)){
@@ -27,42 +32,45 @@ function feather_view_autoload_static($path, $content = '', $view, $opt = array(
 		$array = array();
 
 		//合并map表
-		foreach((array)$opt['resources'] as $resource){
+		foreach($opt['resources'] as $resource){
 			$resource = require($resource);
 			$array = array_merge_recursive($array, $resource);
 		}
 
-		$maps = (array)$array['map'];
+		$maps = $array['map'];
 
 		//拿到当前文件所有的map信息
 		$self_map = _feather_view_autoload_static_get_resources($path, $maps);
 
-		if(!$self_map['isPagelet']){
+		if(!isset($self_map['isPagelet'])){
 			$self_map = array_merge_recursive($array['commonMap'], $self_map);
 		}
 
 		//获取文件的loadjs的map和deps信息
-		$map_deps = _feather_view_autoload_static_get_require_md($self_map['deps'], $maps);
+		$map_deps = isset($self_map['deps']) ? _feather_view_autoload_static_get_require_md($self_map['deps'], $maps) : array();
+		$headJs = isset($self_map['headJs']) ? _feather_view_autoload_static_get_url($self_map['headJs'], $maps, $root) : array();
+		$bottomJs = isset($self_map['bottomJs']) ? _feather_view_autoload_static_get_url($self_map['bottomJs'], $maps, $root) : array();
+		$css = isset($self_map['css']) ? _feather_view_autoload_static_get_url($self_map['css'], $maps, $root) : array();
 
 		$cache = array(
 			'FEATHER_USE_HEAD_SCRIPTS' => array(
-				'outline' => _feather_view_autoload_static_get_url($self_map['headJs'], $maps, $root),
+				'outline' => $headJs,
 				'inline' => array(
-		        	//loadjs配置
+		        	//featherjs配置
 		            'require.config=' . $array['requireConfig'], 
 		            'require.mergeConfig(' . _feather_view_autoload_static_json_encode($map_deps) . ')'
 		        )
 			),
 	        'FEATHER_USE_SCRIPTS' => array(
-	        	'outline' =>  _feather_view_autoload_static_get_url($self_map['bottomJs'], $maps, $root)
+	        	'outline' => $bottomJs
 	        ),
 			'FEATHER_USE_STYLES' => array(
-				'outline' => _feather_view_autoload_static_get_url($self_map['css'], $maps, $root)
+				'outline' => $css
 			)	        
 		);
 
 	    //如果需要设置缓存
-	    if($opt['cache_dir']){
+	    if(isset($opt['cache_dir'])){
 	    	$output = var_export($cache, true);
 	    	$date = date('Y-m-d H:i:s');
 	    	file_put_contents($md5path, "<?php\r\n/*\r\ndate: {$date}\r\nfile: {$path}\r\n*/return {$output};");
@@ -77,25 +85,34 @@ function feather_view_autoload_static($path, $content = '', $view, $opt = array(
 
 //获取所有的静态资源
 function _feather_view_autoload_static_get_resources($path, $maps){
-	$self_map = (array)$maps[$path];
-	$components_map = array();
-
-	foreach((array)$self_map['components'] as $components){
-		$components_map = array_merge_recursive($components_map, _feather_view_autoload_static_get_resources($components, $maps));
+	if(isset($maps[$path])){
+		$self_map = $maps[$path];
+	}else{
+		$self_map = array();
 	}
 
-	return array_merge_recursive($components_map, $self_map);
+	if(isset($self_map['components'])){
+		$components_map = array();
+
+		foreach($self_map['components'] as $components){
+			$components_map = array_merge_recursive($components_map, _feather_view_autoload_static_get_resources($components, $maps));
+		}
+
+		return array_merge_recursive($components_map, $self_map);
+	}
+
+	return $self_map;
 }
 
 //获取静态资源正确的url
 function _feather_view_autoload_static_get_url($resources, $maps, $domain = ''){
 	$tmp = array();
 
-	foreach((array)$resources as $v){
-		$info = $maps[$v];
+	foreach($resources as $v){
+		if(isset($maps[$v])){
+			$info = $maps[$v];
 
-		if($info){
-			if($info['deps']){
+			if(isset($info['deps'])){
 				$tmp = array_merge($tmp, _feather_view_autoload_static_get_url($info['deps'], $maps, $domain));
 			}
 
@@ -110,29 +127,31 @@ function _feather_view_autoload_static_get_url($resources, $maps, $domain = ''){
 
 //获取loadjs的map和deps配置
 function _feather_view_autoload_static_get_require_md($deps, $maps){
-	$deps = (array)$deps;
-	$map_result = array(); $dep_result = array();
+	$map_result = array(); 
+	$dep_result = array();
 	$tmp_deps = array();
 
 	foreach($deps as $m){
-		if($v = $maps[$m]){
+		if(isset($maps[$m])){
+			$v = $maps[$m];
+
 			$url = $v['url'];
 
-			if(!$map_result[$url]){
+			if(!isset($map_result[$url])){
 				$map_result[$url] = array();
 			}
 
 			$map_result[$url][] = $m;
 
 			//deps
-			if($v['isMod']){
-				if($dep = $v['deps']){
-					$dep_result[$m] = $dep;
-				}
-			}
+			if(isset($v['deps'])){
+				$deps = $v['deps'];
 
-			if($dep = $v['deps']){
-				$tmp_deps = array_merge($tmp_deps, $dep);
+				if(isset($v['isMod'])){
+					$dep_result[$m] = $deps;
+				}
+
+				$tmp_deps = array_merge($tmp_deps, $deps);
 			}
 		}
 	}
